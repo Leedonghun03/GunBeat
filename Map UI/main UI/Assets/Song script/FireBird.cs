fbe16f3e02b312618b99a80fdc1b4c85cb192157
LeeDongHun <hanrcboy2@naver.com>
2019-08-12 16:54:41 +0900

    LeeDongHun
    Local 스크립트 작성중

---
+++ Map UI/main UI/Assets/Song script/FireBird.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBird : MonoBehaviour
+{
+    public string Starting;
+
+    public void SaveClick()
+    {
+        PlayerPrefs.SetString("Song", Starting);
+        print("불러오기:" + Starting);
+    }
+    public void ViewClick()
+    {
+        print("불러오기 성공:" + PlayerPrefs.GetString("Song"));
+    }
+}
