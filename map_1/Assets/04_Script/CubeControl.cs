1dc43abd942e4346d5fc5913e3e6bc5f0168bef2
kimjooan <jjaa2017@daum.net>
2019-09-19 11:29:29 +0900

    맥북-김주안
    - map_1폴더안의 유니티 프로젝트 수정
    - 게임 GameUI씬에서 큐브가 자동으로 생성되고 날라오겠금 스크립트 작업
    - 큐브의 생성시간, 속도, 좌표를 수정할 수있도록 변수로 빼놓음.
    - 큐브를 터치시 포인트 획득 스크립트 작업

---
+++ map_1/Assets/04_Script/CubeControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    //큐브속도
+    public float Cubespeed;
+
+    //큐브 없애면 획득포인트
+    public float CubePoint;
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+   
+        if(this.transform.position.z <= 2)
+        {
+            //z좌표가 2일때 삭제
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime * Cubespeed);
+
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+
+            if (true == (Physics.Raycast(ray.origin, ray.direction * 10, out hit)))   //마우스 근처에 오브젝트가 있는지 확인
+            {
+                //더치한 오브젝트 이름과 스크립트 포함한 오브젝트가 같으면 실행
+                if(hit.collider.name==this.gameObject.name)
+                {
+
+                    print(hit.collider.name);
+
+                    GameObject.Find("Manage").GetComponent<GameControl>().GamePointCountFloat = GameObject.Find("Manage").GetComponent<GameControl>().GamePointCountFloat + CubePoint;
+                    GameObject.Find("Manage").GetComponent<GameControl>().GamePointCountText.text = GameObject.Find("Manage").GetComponent<GameControl>().GamePointCountFloat.ToString();
+
+                    //더치시 삭제
+                    Destroy(this.gameObject);
+
+                }
+               
+
+            }
+
+        }
+
+
+
+    }
+}
--- map_1/Assets/04_Script/GameControl.cs
+++ map_1/Assets/04_Script/GameControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
@@ -11,11 +12,35 @@ public class GameControl : MonoBehaviour
     public string GetGunModeString;
     public string GetSongString;
 
+    //생성하려는 오브젝트 
+    public GameObject ContentPObject;
+
+    //생성하려는 오브젝트의 부모오브젝트 (해당 부모오브젝트 밑의 자식으로 들어가야함)
+    public GameObject ContentParanet;
+
+
+
+
+    //전체 게임시간
+    public float TotalGameTime;
+    //큐브 생성시간 텀 
+    public float CubeCraetTime;
+
+    //큐브 시작 점(기준)
+    public float CubeStartPositioZ;
+
+
+    //게임포인트
+    public float GamePointCountFloat;
+    public Text GamePointCountText;
+
+
     void Start()
     {
-       
-        //Key값 : Game Mode
-        GetGameModeString = PlayerPrefs.GetString("GameMode");
+
+
+         //Key값 : Game Mode
+         GetGameModeString = PlayerPrefs.GetString("GameMode");
         //Key값 : Level
         GetLevelString = PlayerPrefs.GetString("Level");
         //Key값 : GunMode
@@ -23,17 +48,49 @@ public class GameControl : MonoBehaviour
         //Key값 : Song
         GetSongString = PlayerPrefs.GetString("Song");
 
+        GamePointCountFloat = 0;
+
+        GamePointCountText.text = GamePointCountFloat.ToString();
+
+        StartCoroutine(GameCubeCreat2e());
 
     }
+
 
     public void HomeGoClick()
     {
 
         Application.LoadLevel("Home");
+       
     }
 
 
+ 
 
+    IEnumerable GameCubeCreate()
+    {
 
+        yield break;
+    }
+
+    IEnumerator GameCubeCreat2e()
+    {
+
+        for(int i=0; i< TotalGameTime; i++)
+        {
+            float RamdomX = Random.Range(-5.0f,5.1f);
+
+            GameObject ContentP = Instantiate(ContentPObject);
+            ContentP.transform.parent = ContentParanet.transform;
+            ContentP.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            ContentP.transform.position = new Vector3(RamdomX, 2.8f, CubeStartPositioZ);
+
+            ContentP.name = i.ToString();
+
+            yield return new WaitForSeconds(CubeCraetTime);
+        }
+
+        yield break;
+    }
 
 }
