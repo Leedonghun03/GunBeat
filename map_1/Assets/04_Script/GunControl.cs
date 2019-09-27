85dd2ef8c97936fc2fdc135377f582fd51e2cd38
JooYoungMin <51253756+JooYoungMin@users.noreply.github.com>
2019-09-27 19:47:41 +0900

    LeeDongHun
    -vr 트레거 총 적용
    - 총 컨트롤러 스크립트 작성중

---
+++ map_1/Assets/04_Script/GunControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+public class GunControl : MonoBehaviour
+{
+
+
+    public SteamVR_TrackedObject mTracjedObj;
+    private Hand hand;
+
+    
+    void Start()
+    {
+        hand = gameObject.GetComponent<Hand>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
